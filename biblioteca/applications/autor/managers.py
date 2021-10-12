d04d1c17e84eb9dbc12a46533b0e3cdabf415343
lea.arturi <lea.arturi@gmail.com>
2021-10-12 14:01:45 -0300

    Busqueda de Autor por Nombre y Apellido

--- biblioteca/applications/autor/managers.py
+++ biblioteca/applications/autor/managers.py
@@ -1,6 +1,21 @@
 from django.db import models
-
+from django.db.models import Q
 class AutorManager(models.Manager):
     
-    def listar_autores(self):
-        return self.all()
+    def buscar_autor_nombre(self, kword):
+
+        result = self.filter(
+            nombre__icontains=kword
+        )
+
+        return result
+
+    def buscar_autor_apenom(self, kword):
+
+        result = self.filter(
+           Q(nombre__icontains = kword) | Q(apellido__icontains = kword)
+        )
+
+        return result
+
+    
