7060eca6aece4f7ec2dfd159514f5eaadebe7a3a
lea.arturi <lea.arturi@gmail.com>
2021-10-17 15:59:05 -0300

    Prod

--- biblioteca/biblioteca/settings/prod.py
+++ biblioteca/biblioteca/settings/prod.py
@@ -27,5 +27,15 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.parent/'media/'
